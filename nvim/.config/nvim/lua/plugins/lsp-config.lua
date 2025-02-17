0e3bc69af573988b2a65bf3d60d18d336cd70944
drewgoodwin <drewgoodwin@gmail.com>
2025-02-17 21:03:32 +1000

    Put vim.lsp.buf.definition behind <leader>gd, instead of just gd

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -21,7 +21,7 @@ return {
       lspconfig.ts_ls.setup({})
       lspconfig.marksman.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n','v'}, '<leader>gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
       vim.keymap.set({'n', 'v' }, '<leader>rn', vim.lsp.buf.rename, {})
       vim.keymap.set({'n'}, '<leader>e', vim.diagnostic.open_float, {})
