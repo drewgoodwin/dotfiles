767004ecfabe1ffe7ac3c1f29023a07d6420d981
drewgoodwin <drewgoodwin@gmail.com>
2025-05-02 21:01:33 +1000

    Added eslint_d and lspkind plugins to neovim

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,14 +1,19 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
+      debug = true,
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.erb_lint,
         null_ls.builtins.diagnostics.rubocop,
-          null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.rubocop,
+        require("none-ls.diagnostics.eslint_d"),
       },
     })
 
