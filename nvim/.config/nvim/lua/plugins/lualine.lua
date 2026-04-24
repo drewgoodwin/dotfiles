dae04085292ad1f66160b3ee4bd8c34c6607e2e9
drewgoodwin <drewgoodwin@gmail.com>
2026-04-24 20:24:10 +1000

    made the background of lualine more visible so it is easier to see where splits are in a neovim session

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,12 +2,31 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local function make_theme()
+			local custom_gruvbox = vim.deepcopy(require("lualine.themes.gruvbox-material"))
+			custom_gruvbox.normal.c.bg = "#3c3836"
+			custom_gruvbox.inactive.a.bg = "#3c3836"
+			return custom_gruvbox
+		end
+
 		require("lualine").setup({
 			options = {
-				theme = "gruvbox-material",
+				theme = make_theme(),
 				component_separators = "|",
 				section_separators = "",
 			},
+		})
+
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			callback = function()
+				require("lualine").setup({
+					options = {
+						theme = make_theme(),
+						component_separators = "|",
+						section_separators = "",
+					},
+				})
+			end,
 		})
 	end,
 }
