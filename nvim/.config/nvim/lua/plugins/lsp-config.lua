8a687d9128beb88932ffbbe0ed5f54d755eed743
drewgoodwin <drewgoodwin@gmail.com>
2026-05-15 21:18:48 +1000

    Updated hyprland config to lua

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -56,7 +56,10 @@ return {
 							globals = { "vim" },
 						},
 						workspace = {
-							library = vim.api.nvim_get_runtime_file("", true),
+							library = { 
+                vim.api.nvim_get_runtime_file("", true),
+                "/usr/share/hypr/stubs/hl.meta.lua",
+              },
 							checkThirdParty = false,
 						},
 						telemetry = {
